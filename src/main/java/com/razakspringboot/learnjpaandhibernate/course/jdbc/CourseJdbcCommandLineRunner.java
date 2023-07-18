dd2fe4f62b9fb8ddfa2d855df6265c7a7797eb62
Abdul-Razak Hussein <abdul-razak.hussein@amalitech.com>
2023-07-18 13:36:46 +0000

    Add setters to the Course class and test with Command line runner

--- src/main/java/com/razakspringboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/razakspringboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.razakspringboot.learnjpaandhibernate.course.jdbc;
 
+import com.razakspringboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,18 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception
     {
-         repository.insert();
+        String author = "in28minutes";
+         repository.insert(new Course(1, "Learn AWS Now!", author));
+         repository.insert(new Course(2, "Learn Azure", author));
+         repository.insert(new Course(3, "Learn DevOps", author));
+         repository.insert(new Course(4, "Learn AWS Full Stack", author));
+         repository.insert(new Course(5, "Learn Cloud Computing", author));
+         repository.insert(new Course(6, "Learn Java", author));
+
+       repository.deleteById(1);
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
+
 
     }
 }
--- src/main/java/com/razakspringboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/razakspringboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,6 +1,8 @@
 package com.razakspringboot.learnjpaandhibernate.course.jdbc;
 
+import com.razakspringboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -9,14 +11,35 @@ public class CourseJdbcRepository
 {
    @Autowired
     private JdbcTemplate springJdbcTemplate;
-    private String INSERT_QUERY =
+    private static String INSERT_QUERY =
             """
                         insert into course(id, name, author) 
-                        values(1, 'Learn AWS', 'in28minutes');
+                        values(?, ?, ?);
              """;
 
-    public void insert()
+    private static String DELETE_QUERY =
+            """
+               delete from course where id = ?
+            """;
+
+    private static String SELECT_QUERY =
+            """
+               select * from course 
+               where id = ?
+            """;
+    public void insert(Course course)
     {
-        springJdbcTemplate.update(INSERT_QUERY);
+        springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+    }
+
+    public void  deleteById(long id)
+    {
+        springJdbcTemplate.update(DELETE_QUERY, id);
+    }
+
+    public Course findById(long id)
+    {
+       return springJdbcTemplate.queryForObject(SELECT_QUERY,
+                new BeanPropertyRowMapper<>(Course.class), id);
     }
 }

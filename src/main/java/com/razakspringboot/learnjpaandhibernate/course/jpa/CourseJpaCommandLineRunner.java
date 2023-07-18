372ecd12d4693fe562fc5ade857eacb9fdf73f44
Abdul-Razak Hussein <abdul-razak.hussein@amalitech.com>
2023-07-18 14:43:40 +0000

    Configure the JPA and test it commandline runner

---
+++ src/main/java/com/razakspringboot/learnjpaandhibernate/course/jpa/CourseJpaCommandLineRunner.java
@@ -0,0 +1,31 @@
+package com.razakspringboot.learnjpaandhibernate.course.jpa;
+
+import com.razakspringboot.learnjpaandhibernate.course.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJpaCommandLineRunner implements CommandLineRunner
+{
+
+    @Autowired
+    private CourseJpaRepository repository;
+
+    public void run(String... args) throws Exception
+    {
+        String author = "in28minutes";
+        repository.insert(new Course(1, "Learn AWS Now!", author));
+        repository.insert(new Course(2, "Learn Azure", author));
+        repository.insert(new Course(3, "Learn JS", author));
+        repository.insert(new Course(4, "Learn AWS Full Stack", author));
+        repository.insert(new Course(5, "Learn Cloud Computing", author));
+        repository.insert(new Course(6, "Learn Java", author));
+
+        repository.deleteById(1);
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
+
+
+    }
+}
---
+++ src/main/java/com/razakspringboot/learnjpaandhibernate/course/jpa/CourseJpaRepository.java
@@ -0,0 +1,33 @@
+package com.razakspringboot.learnjpaandhibernate.course.jpa;
+
+import com.razakspringboot.learnjpaandhibernate.course.Course;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class CourseJpaRepository
+{
+
+    @PersistenceContext // or @Autowired
+    private EntityManager entityManager;
+
+    public void insert(Course course)
+    {
+        entityManager.merge(course);
+    }
+
+    public Course findById(long id)
+    {
+        return entityManager.find(Course.class, id);
+    }
+
+    public void deleteById(long id)
+    {
+        Course course = entityManager.find(Course.class, id);
+        entityManager.remove(course);
+
+    }
+}
